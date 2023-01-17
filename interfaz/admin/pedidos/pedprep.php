aff82c6dc16ae91802cacba8fde4dfee51b5732d
ddelafuente78 <tokenlabs@gmail.com>
2023-01-17 00:16:01 -0300

    Agregamos pantalla pedidos en preparacion

---
+++ interfaz/admin/pedidos/pedprep.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Administrador - compostela</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+      <style>
+        body {
+              font-family: "Segoe UI", sans-serif;
+              font-size:100%;
+          }
+
+        .menu {
+            background:#333;
+            width:100%;
+            height: 100%;
+            left: 10px;
+          }
+
+          #contenido {
+            margin-left:0;
+          }
+
+          .sidebar {
+            position: fixed;
+            top: -10px;
+            bottom: 10px;
+            left: 0px ;
+            z-index: 1;
+          }
+
+          .sidebar ul, .sidebar li {
+            margin:0;
+            padding:0;
+            margin-left: 10px ;
+            list-style:none inside;
+          }
+
+          .sidebar ul {
+            margin: 4rem auto;
+            display: block;
+            width: 80%;
+            min-width:120px;
+          }
+
+          .sidebar a {
+            display: block;
+            font-size: 120%;
+            color: #fff;
+            text-decoration: none;
+          }
+
+          .sidebar a:hover{
+            color:#fff;
+            background-color: #f90;
+            border-radius: 5px;
+            padding-left: 5px;
+          }
+
+          #contenido {
+           margin-left: 150px;
+          }
+
+          #menuhorizontal {
+            margin:0;
+            padding:0;
+            list-style-type:none; 
+          }
+          #menuhorizontal a {
+            width:100px;
+            text-decoration:none;
+            text-align:center;
+            color:#ff0000;
+            background-color:#f7f8e8;
+            padding:3px 5px;
+            border-right:1px solid blue;
+            display:block;
+            width: 150px;
+          }
+
+          #menuhorizontal li {
+            float:left;
+          }
+
+          #menuhorizontal a:hover {
+            background-color:#336699;
+          }
+      </style>
+    </head>
+    <body>
+      
+      <?php
+        include '../../../helper/conexion.php';
+        include '../../../helper/validar_usuario.php';
+
+
+        $pedidosprearando = mysqli_query($conexion, "SELECT pc.codigo, pc.fecha_entrega,
+                                        if(prioridad_urgente = 1,'Urgente','Normal') as prioridad, 
+                                        d.razon_social, u.nombre
+                                        FROM compostela.pedidoscab pc 
+                                        join estados e on pc.estado_id = e.id
+                                        join destinatarios d on pc.destinatario_id = d.id
+                                        join usuarios u on pc.usuario_id = u.id
+                                        where estado_id =1
+                                        order by prioridad_urgente desc, fecha_entrega;");
+      ?>
+      <div class='Container'>
+        <div class='row'>
+          <div class="col-12">
+            <!-- Header -->
+            <header class="d-flex flex-wrap py-3 mb-5 border-bottom">
+              <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <div class="container">
+                  <a class="navbar-brand" href="#">usuario:</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="menu.php">Cerrar</a>                    
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            </header>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-1">
+            <div id="sidebar" class="sidebar">
+              <ul class="menu">
+                <li><a href="#">Pedidos</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Usuarios</a></li>
+                <li><a href="#">Reportes</a></li>
+                <li><a href="#">Opcion 5</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-11">
+            <div class="container-fluid">
+              <div class="row">
+                <ul id="menuhorizontal">
+                  <li><a href="#">En preparacion</a></li>
+                  <li><a href="#">Despachado</a></li>
+                  <li><a href="#">Entregado</a></li>
+                </ul>
+              </div>
+              <div class="row">
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Codigo</th>
+                      <th>Prioridad</th>
+                      <th>fecha entrega</th>  
+                      <th>destinatario</th>
+                      <th>usuario</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                      foreach ($pedidosprearando as $fila) { 
+                    ?>
+                      <tr>
+                        <td><?php echo $fila['codigo'] ?></td>
+                        <td><?php echo $fila['prioridad'] ?></td>
+                        <td><?php echo $fila['fecha_entrega'] ?></td>
+                        <td><?php echo $fila['razon_social'] ?></td>
+                        <td><?php echo $fila['nombre'] ?></td>
+
+                        <td class="text-right">
+                            <a class="btn btn-outline-danger btn-sm botonborrar" id="btnBorrar<?php echo $fila['codigo'] ?>" role="button" href="#" 
+                              onClick="getButtontoOpen(<?php echo $fila['codigo'] ?>)">Actualizar...</a>
+                        </td>
+                        <?php
+                          }
+                        ?>
+                      </tr>
+                  </tbody>
+                </table> 
+              <div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+</html> 
